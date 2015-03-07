02ddb9f114d84a355594c55bf428dfe922408cdb
paytoNZ <juanlp01@ucm.es>
2015-03-07 12:16:06 +0100

    picasa added and cambios en splash y contact fragment

--- app/src/main/java/com/underphones/smartfestival/ContactsFragment.java
+++ app/src/main/java/com/underphones/smartfestival/ContactsFragment.java
@@ -1,14 +1,19 @@
 package com.underphones.smartfestival;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class ContactsFragment extends Fragment {
+
+    private ListView contactListView;
 
     public ContactsFragment() {
         // Required empty public constructor
@@ -23,7 +28,13 @@ public class ContactsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        contactListView = (ListView) rootView.findViewById(R.id.contact_list_view);
+        ContactsAdapter ca = new ContactsAdapter(getActivity(), new ArrayList<ContactBean>());
+        contactListView.setAdapter(ca);
+
+        return rootView;
     }
 
 
--- app/src/main/java/com/underphones/smartfestival/SplashActivity.java
+++ app/src/main/java/com/underphones/smartfestival/SplashActivity.java
@@ -1,27 +1,35 @@
 package com.underphones.smartfestival;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 
 public class SplashActivity extends ActionBarActivity {
 
 
     private static final String TAG = "SplashActivity";
+    private ImageView splashImage;
     private AsyncTask<Void, Void, Void> task;
     private TextView debug;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         debug = (TextView) findViewById(R.id.textViewDevelop);
-
+        splashImage = (ImageView) findViewById(R.id.imageView);
+        context = (Activity) this;
         if (BuildConfig.DEBUG)
             debug.setVisibility(View.VISIBLE);
         else
@@ -32,7 +40,11 @@ public class SplashActivity extends ActionBarActivity {
             protected Void doInBackground(Void... params) {
                 //Check server and database
                 try {
-                    Thread.sleep(2000);
+
+                    Picasso.with(context).load("").centerCrop().into(splashImage);
+                    Thread.sleep(100);
+
+
                 } catch (Exception e) {
                 }
                 return null;
